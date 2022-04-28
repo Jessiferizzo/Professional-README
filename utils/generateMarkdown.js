e42726f58c16c8abc25ba5fad1778fba3f5db825
Zoey <jsisavath@gmail.com>
2022-04-28 03:06:41 -0500

    edited README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -49,24 +49,6 @@ function generateMarkdown(data) {
   
   Copyright (c) 2022 Jessica Sisavath
   
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-  
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-
   ## Contributing
   ${data.contributing}
   
@@ -76,7 +58,7 @@ function generateMarkdown(data) {
   ## Questions 📫
   Please contact me using the following links:
   [GitHub](https://github.com/${data.githubname})
-  
+
   [Email: ${data.email}](mailto:${data.email})
   
   ## Acknowledgments
